f749a95034deecfc4d58171d269a180c46885829
Danylo Lubenets <danilarff86@gmail.com>
2021-01-30 16:21:33 +0200

    switched to ckeckpoint

--- server/index.js
+++ server/index.js
@@ -9,66 +9,63 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// Postgress Client Setup
+// Postgres Client Setup
 const { Pool } = require('pg');
 const pgClient = new Pool({
-    user: keys.pgUser,
-    host: keys.pgHost,
-    port: keys.pgPort,
-    database: keys.pgDatabase,
-    password: keys.pgPassword
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort,
 });
 
-pgClient.on('error', () => console.log('Lost PG connection'));
-
-pgClient
-    .query('CREATE TABLE IF NOT EXISTS values(number INT)')
+pgClient.on('connect', () => {
+  pgClient
+    .query('CREATE TABLE IF NOT EXISTS values (number INT)')
     .catch((err) => console.log(err));
+});
 
 // Redis Client Setup
-
 const redis = require('redis');
-
 const redisClient = redis.createClient({
-    host: keys.redisHost,
-    port: keys.redisPort,
-    retry_strategy: () => 1000
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000,
 });
-
 const redisPublisher = redisClient.duplicate();
 
-// Express Route Handlers
+// Express route handlers
 
-app.get('/', (req, resp) => {
-    resp.send('Hi');
+app.get('/', (req, res) => {
+  res.send('Hi');
 });
 
-app.get('/values/all', async (req, resp) => {
-    const values = await pgClient.query('SELECT * FROM values');
+app.get('/values/all', async (req, res) => {
+  const values = await pgClient.query('SELECT * from values');
 
-    resp.send(values.rows);
+  res.send(values.rows);
 });
 
-app.get('/values/current', async (req, resp) => {
-    redisClient.hgetall('values', (err, values) => {
-        resp.send(values);
-    });
+app.get('/values/current', async (req, res) => {
+  redisClient.hgetall('values', (err, values) => {
+    res.send(values);
+  });
 });
 
-app.post('/values', async (req, resp) => {
-    const index = req.body.index;
+app.post('/values', async (req, res) => {
+  const index = req.body.index;
 
-    if (parseInt(index)) {
-        return resp.status(422).send('Index too high');
-    }
+  if (parseInt(index) > 40) {
+    return res.status(422).send('Index too high');
+  }
 
-    redisClient.hset('values', index, 'Nothing yet!');
-    redisPublisher.publish('insert', index);
-    pgClient.query('INSERT INTO values(number) VALUES ($1)', [index]);
+  redisClient.hset('values', index, 'Nothing yet!');
+  redisPublisher.publish('insert', index);
+  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
 
-    resp.send({ working: true });
+  res.send({ working: true });
 });
 
-app.listen(5000, err => {
-    console.log('Listening');
+app.listen(5000, (err) => {
+  console.log('Listening');
 });
